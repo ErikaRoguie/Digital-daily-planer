19b67e1fd9236a5865c936b636b7367b0be2636c
Erika <88861951+ErikaRoguie@users.noreply.github.com>
2022-02-05 13:23:41 -0700

    corrections

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,29 +6,30 @@ console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 // times are from 6-10( 17 time blocks)
 let dt = moment();
 dt.format("HH:mm");
-var line = ["6","7","8","9", "10", "11", "12", "13", "14", "15", "16","17","18","19","20","21","22"]
+var line = ["6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22"]
 console.log(line)
 
 
 
 for (let i = 0; i < line.length; i++) {
     var id = "#" + line[i]
-var storage = localStorage.getItem(line[i])
-   $(id).children(".col-10").children("textarea").val(storage);
-        if (line[i] == moment().hours()) {
-      $(id).addClass('present')
-        } else if(line[i] < moment().hours()) {
-            $(id).addClass("past")
-        }
-    
-    
-     else {
+    var storage = localStorage.getItem(line[i])
+    $(id).children("textarea").val(storage);
+    if (line[i] == moment().hours()) {
+        $(id).addClass('present')
+    } else if (line[i] < moment().hours()) {
+        $(id).addClass("past")
+    }
+
+
+    else {
         $(id).addClass("future")
-        }
-    };
-$(".saveBtn").on("click", function() {
- var data=$(this).parent().siblings(".col-10").children("textarea").val();
-    var hour=$(this).parent().parent().attr("id")
+    }
+};
+$(".saveBtn").on("click", function () {
+    var data = $(this).siblings("textarea").val();
+    var hour = $(this).parent().attr("id")
+    console.log(hour, data)
     localStorage.setItem(hour, data);
 })
 
